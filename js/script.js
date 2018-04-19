2a079cb79d9ed8fd164631795d01e4061a5100f6
Autumn <autumncbullard@gmail.com>
2018-04-19 12:48:18 -0400

    Add event listeners that will close the modal when the Cancel button is clicked or when the user clicks outside the modal

--- js/script.js
+++ js/script.js
@@ -10,3 +10,14 @@ function Book(title, author, pages, read) {
 document.querySelector('.add-book').addEventListener('click', () => {
   document.getElementById('modal').style.display = 'block';
 });
+
+document.querySelector('.cancel').addEventListener('click', () => {
+  document.getElementById('modal').style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+
+  if (event.target.id === 'modal') {
+    document.getElementById('modal').style.display = 'none';
+  }
+});
